278ed3ed0d2c5ca3c1e8bb16fe2212aecc9383e5
Tad Donaghe <terry.donaghe@gmail.com>
2013-04-27 12:47:01 -0700

    Beginnings of test framework set up

---
+++ src/server/_nodeFlickrToy_test.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: TDonaghe
+ * Date: 4/27/13
+ * Time: 12:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+(function() {
+   "use strict";
+    exports.test_nothing = function(test) {
+        console.log("Test nothing");
+        test.done();
+    };
+})();
